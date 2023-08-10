93ee5d471d11b56126bc675f37e9095ab5db4d29
Diego <105765223+0xfuturistic@users.noreply.github.com>
2023-08-11 01:12:16 +0200

    Update AppsManager.sol

--- src/AppsManager.sol
+++ src/AppsManager.sol
@@ -1,44 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {BaseAccount, UserOperation} from "erc4337/core/BaseAccount.sol";
-
-type App is address;
-
-library AppLib {
-    function run(App app, bytes memory data) internal view {
-        (bool success, ) = App.unwrap(app).staticcall(
-            abi.encodeWithSignature("run(bytes calldata)", data)
-        );
-
-        if (!success) {
-            revert("AppLib: app execution failed");
-        }
-    }
-}
+import "./Types.sol";
 
 contract AppsManager {
-    using AppLib for App;
-    App[] public apps;
+    Apps apps;
 
-    function _runApps(bytes memory data) internal view {
-        for (uint i = 0; i < apps.length; i++) {
-            _runApp(apps[i], data);
-        }
-    }
-
-    function _runApp(App app, bytes memory data) internal view {
-        app.run(data);
-    }
-
-    function _newApps(App[] calldata app) internal {
-        for (uint i = 0; i < app.length; i++) {
-            _newApp(app[i]);
-        }
-    }
-
-    function _newApp(App app) internal {
-        apps.push(app);
+    modifier requireEnoughBalance() {
+        _requireEnoughBalance();
+        _;
     }
 
     function _requireEnoughBalance() internal view {
