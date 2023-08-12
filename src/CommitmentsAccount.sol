0e3cf2348181658f1b9d119ac13587885bc6fb30
Diego <105765223+0xfuturistic@users.noreply.github.com>
2023-08-12 02:30:09 +0200

    change back to evaluate rather than prove

--- src/CommitmentsAccount.sol
+++ src/CommitmentsAccount.sol
@@ -3,14 +3,10 @@ pragma solidity ^0.8.20;
 
 import {BaseAccount, UserOperation} from "erc4337/core/BaseAccount.sol";
 
-struct Commitment {
-    function (bytes memory) external view prove;
-}
-
 /// @title CommitmentAccount
 /// @dev Contract for managing user commitments and validating user operations.
 abstract contract CommitmentsAccount is BaseAccount {
-    /// @dev Validates user operation and checks if the user can prove the commitments.
+    /// @dev Validates user operation and checks if the user can evaluate the commitments.
     /// @param userOp User operation to be validated.
     /// @param userOpHash Hash of the user operation.
     /// @param missingAccountFunds Amount of missing account funds.
@@ -27,43 +23,47 @@ abstract contract CommitmentsAccount is BaseAccount {
         _payPrefund(missingAccountFunds);
 
         /// @dev We first check if the user has enough funds to pay for the worst case resource usage.
-        //       That is, we assume that the user will prove the most expensive series of commitments.
+        //       That is, we assume that the user will evaluate the most expensive series of commitments.
         uint256 gasPreValiation;
         require(
             address(this).balance >= (gasPreValiation = gasleft()) * tx.gasprice,
             "Not enough eth for worst case validation"
         );
 
-        /// @dev We now check if the user can prove the commitments. We use staticcall as a proxy so
+        /// @dev We now check if the user can evaluate the commitments. We use staticcall as a proxy so
         //       that the main call doesn't revert if proving fails without having reimbursed the gas
         ///      used for validation.
         (bool success,) = address(this).staticcall(
             abi.encodeWithSelector(
-                this.proveUserCommitments.selector,
+                this.evaluateUserCommitments.selector,
                 userOp.sender,
                 abi.encode(userOp, userOpHash, missingAccountFunds, validationData)
             )
         );
 
-        /// @dev If the user can't prove the commitments, we refund the user for the gas used for validation.
+        /// @dev If the user can't evaluate the commitments, we refund the user for the gas used for validation.
         ///      This is done as an alternative to reverting the transaction and msg.sender incurring the
-        ///      opportunity cost from the resources incurred by trying to prove the commitments on behalf
+        ///      opportunity cost from the resources incurred by trying to evaluate the commitments on behalf
         ///      of the user.
         if (!success) payable(msg.sender).transfer((gasPreValiation - gasleft()) * tx.gasprice);
     }
 
-    /// @dev Sample implementation of what ProveUserCommitments could look like.
+    /// @dev Sample implementation of what evaluateUserCommitments could look like.
     /// @param user User address.
     /// @param extraData Extra data to be used for proving commitments.
-    function proveUserCommitments(address user, bytes memory extraData) external view virtual {
-        Commitment[] memory commitments = _getUserCommitments(user);
+    function evaluateUserCommitments(address user, bytes memory extraData) external view virtual {
+        function (bytes memory) external view[] memory commitments = _getUserCommitments(user);
         for (uint256 i = 0; i < commitments.length; i++) {
-            commitments[i].prove(extraData);
+            commitments[i](extraData);
         }
     }
 
-    /// @dev Gets user commitments.
+    /// @dev Gets the functions to evaluate a user's commitments
     /// @param user User address.
-    /// @return userCommitments Returns user commitments.
-    function _getUserCommitments(address user) internal view virtual returns (Commitment[] memory);
+    /// @return userCommitments Returns the functions to evaluate each user's commitments
+    function _getUserCommitments(address user)
+        internal
+        view
+        virtual
+        returns (function (bytes memory) external view[] memory userCommitments);
 }
