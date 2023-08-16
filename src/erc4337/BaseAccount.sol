425b909565cddfca3035df70bd104f2cde4df8e1
Diego <0xfuturistic@pm.me>
2023-08-17 01:31:05 +0200

    Update BaseAccount.sol

--- src/erc4337/BaseAccount.sol
+++ src/erc4337/BaseAccount.sol
@@ -3,9 +3,12 @@ pragma solidity ^0.8.15;
 
 import {BaseAccount as ERC4337BaseAccount, UserOperation} from "erc4337/core/BaseAccount.sol";
 import {ConstraintsManager} from "../ConstraintsManager.sol";
+import "../lib/types.sol";
 
 abstract contract BaseAccount is ConstraintsManager, ERC4337BaseAccount {
-    uint256 constant CONSTRAINTS_GAS_LIMIT = 100;
+    uint256 public constant CONSTRAINTS_GAS_LIMIT = 1 ether;
+
+    constructor(address constraintsAdder) ConstraintsManager(constraintsAdder) {}
 
     function validateUserOp(UserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
         external
@@ -15,11 +18,8 @@ abstract contract BaseAccount is ConstraintsManager, ERC4337BaseAccount {
         _requireFromEntryPoint();
         validationData = _validateSignature(userOp, userOpHash);
         _validateNonce(userOp.nonce);
-        _requireConstraintsSatisfied(userOp);
+        /// @dev new line
+        _requireConstraintsAreSatisfied(abi.encode(userOp), CONSTRAINTS_GAS_LIMIT);
         _payPrefund(missingAccountFunds);
-    }
-
-    function _requireConstraintsSatisfied(UserOperation calldata userOp) internal {
-        require(_areConstraintsAllSatisfied(abi.encode(userOp), CONSTRAINTS_GAS_LIMIT));
     }
 }
